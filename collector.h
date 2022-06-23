9b4bb5b064cd0e69cb78686a4089117a3ef973cf
Said Mia <smia@e3r11p10.1337.ma>
2022-06-23 20:01:53 +0100

    done

--- collector.h
+++ collector.h
@@ -6,7 +6,7 @@
 /*   By: smia <smia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:44:18 by smia              #+#    #+#             */
-/*   Updated: 2022/06/23 19:52:42 by smia             ###   ########.fr       */
+/*   Updated: 2022/06/23 19:59:52 by smia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+
+// linkedlist methode
 
 typedef struct collector
 {
@@ -28,4 +30,11 @@ t_collector	**append_adr(t_collector **root, void *adr);
 void		*my_malloc(t_collector **root, size_t size);
 void		ft_collect(t_collector **root, t_collector *node);
 
+//array methode
+
+int			lst_size(void **lst);
+void		*ft_malloc_array(void ***lst, size_t size);
+void		append_array(void ***list, void *adr);
+void		lst_free(void **lst);
+int			lst_size(void **lst);
 #endif
